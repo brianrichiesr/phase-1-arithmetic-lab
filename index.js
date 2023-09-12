2b5140be5327c288bb31b44c8b0cc64664dff2a9
Brian Richie Sr <brianrichiesr@gmail.com>
2023-09-11 22:41:57 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 20;
+let num4 = 16;
+let arr = new Set();
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let mod = num3 % num4;
+
+arr.add(num3);
+arr.add(num2);
+arr.add(num4);
+
+let max = Math.max(...arr);
